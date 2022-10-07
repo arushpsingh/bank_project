d3ae8aad2bdee7223c8ac2f328686aec6657f887
Arush <arush.singh@nagarro.com>
2022-10-08 03:19:47 +0530

    GS added

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,9 +8,11 @@ import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
+import GetStarted from "./GetStarted";
 
 export {
   Navbar,
+  GetStarted,
   Billing,
   CardDeal,
   Business,
