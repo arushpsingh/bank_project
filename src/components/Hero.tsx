46e4aaa194f0b85c2eff2a40838c2522674c91f5
Arush <arush.singh@nagarro.com>
2022-10-09 18:52:47 +0530

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,9 @@ const Hero = () => {
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
       </div>
 
-      <div className={`ss`}></div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
     </section>
   )
 }
