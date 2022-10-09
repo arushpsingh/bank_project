b4b6e191b17b7b896a6beb53a560c6e704dfc0f0
Arush <arush.singh@nagarro.com>
2022-10-09 18:49:01 +0530

    fixed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
 import Navbar from "./Navbar";
-=======
-
-import Hero from "./Hero";
-import GetStarted from "./GetStarted";
->>>>>>> test
 
 export {
-<<<<<<< HEAD
-  Hero
-=======
-  Navbar,
-<<<<<<< HEAD
-=======
-  GetStarted,
-  Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testimonials,
-  Hero,
->>>>>>> test
->>>>>>> test
+  Navbar
 }
