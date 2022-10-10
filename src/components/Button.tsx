4e5fe76ccfe4e0c64e8aa187131eee48f5015619
Arush <arush.singh@nagarro.com>
2022-10-10 22:02:48 +0530

    Test added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import styles from '../style'
 
 const Button = (props: any) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none  ${styles} rounded-lg mt-10`}>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none  ${styles} rounded-[10px] mt-10`}>
       {props.text}
     </button>
   )
