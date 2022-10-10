8d7e485c1dc0539802e3110bca78b9f446afd230
Arush <arush.singh@nagarro.com>
2022-10-10 21:27:25 +0530

    Billing added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from '../style'
 
-const Button = () => {
+const Button = (props: any) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none  ${styles} rounded-lg mt-10`}>
+      {props.text}
+    </button>
   )
 }
 
-export default Button
+export default Button;
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -21,7 +21,7 @@ export const navLinks = [
     title: "Clients",
   },
 ];
-
+ 
 export const features = [
   {
     id: "feature-1",
